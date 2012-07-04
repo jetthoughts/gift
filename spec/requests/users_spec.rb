643c47cafad8ef6e6d08f026f4f4e76e02df8e01
Maxim Litvinov <mli@jetthoughts.com>
2012-07-04 18:17:01 +0300

    add test sign up

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
-feature "Signing up" do
+feature "User sign in and sign up actions" do
   background do
-    @user = Fabricator(:user, email: 'test@test.com', password: 'qweqwe')
+    #user = Fabricator(:user, email: 'test@test.com', password: 'qweqwe')
+    visit root_path
   end
 
   scenario "Signing in with incorrect credentials" do
-    visit root_path
-
     fill_in 'Email', with: 'test@test.com'
     fill_in 'Password', with: 'incorrect'
     click_button 'Sign in'
 
     page.should have_content 'Invalid email or password'
+  end
+
+  scenario "Signing up" do
+      click_link 'Sign up'
+      page.should have_content 'Sign up'
+
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'qweqwe'
+      fill_in 'Password confirmation', with: 'qweqwe'
+      click_button 'Sign up'
+
+      current_page? == root_path
+      page.should have_content 'You need to sign in or sign up before continuing.'
+
   end
 end
