a1f3c1abe30f2e49b74a1bf3edcf0f1a96e6d013
Sergey Andreyev <san@jetthoughts.com>
2012-07-05 14:33:33 +0300

    Generate scaffold for project

---
+++ spec/fabricators/project_fabricator.rb
@@ -0,0 +1,9 @@
+Fabricator(:project) do
+  name "MyString"
+  description "MyString"
+  article_link "MyString"
+  participants_add_own_suggestions false
+  fixed_amount 1.5
+  opend_end "2012-07-05 14:32:18"
+  paid_type "MyString"
+end
