c797f89c2e476fc30c460e2b12c3ea9de209bb79
Sergey Andreyev <san@jetthoughts.com>
2012-07-05 15:08:57 +0300

    Update projects controller after scaffold generation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,83 +1,67 @@
 class ProjectsController < ApplicationController
+  has_scope :page, default: 1
+
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @projects }
-    end
+    respond_with @projects = scoped_chain.all
   end
 
   # GET /projects/1
   # GET /projects/1.json
   def show
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @project }
-    end
+    respond_with @project = chain.find(params[:id])
   end
 
   # GET /projects/new
   # GET /projects/new.json
   def new
-    @project = Project.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @project }
-    end
+    respond_with @project = chain.new
   end
 
   # GET /projects/1/edit
   def edit
-    @project = Project.find(params[:id])
+    respond_with @project = chain.find(params[:id])
   end
 
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(params[:project])
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @project = chain.create(project_params(user: current_user))
   end
 
   # PUT /projects/1
   # PUT /projects/1.json
   def update
-    @project = Project.find(params[:id])
-
-    respond_to do |format|
-      if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with @project = chain.update(params[:id], project_params)
   end
 
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
+    respond_with @project = chain.destroy(params[:id])
+  end
 
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
-    end
+
+  private
+
+  def project_params new_params = {}
+    params[:project].merge new_params
+  end
+
+  def owner
+    @owner ||= current_user
+  end
+
+  def projects_chain
+    @rojects_chain ||= (owner and owner.projects) or Project
+  end
+
+  def chain
+    projects_chain.ordered_by_date
+  end
+
+  def scoped_chain
+    apply_scopes chain
   end
 end
