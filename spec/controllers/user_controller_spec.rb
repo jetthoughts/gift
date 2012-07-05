75a2b37a0d43afe13b1b222777e1ac322e391413
Sergey Andreyev <san@jetthoughts.com>
2012-07-05 14:22:43 +0300

    Fix controller test for users controller

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UsersController do
   describe "GET show" do
     it "assigns the requested user as @user" do
-      user = Fabricate(:user)
+      user = Fabricate(:user, confirmed_at: DateTime.now)
       sign_in user
 
       get :show
