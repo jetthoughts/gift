657a2e5a3727ec19036927ef691c76c7a96c6c90
Sergey Andreyev <san@jetthoughts.com>
2012-07-06 17:00:08 +0300

    Add unit tests for project model

--- spec/fabricators/project_fabricator.rb
+++ spec/fabricators/project_fabricator.rb
@@ -3,7 +3,5 @@ Fabricator(:project) do
   description "MyString"
   article_link "MyString"
   participants_add_own_suggestions false
-  fixed_amount 1.5
-  opend_end "2012-07-05 14:32:18"
-  paid_type "MyString"
+  paid_type :pay_pal
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#end_type" do
+    it "should be valid if eql to fixed_amount" do
+      Fabricate.build(:project, end_type: 'fixed_amount').should_not have(1).errors_on(:end_type)
+    end
+    it "should be valid if eql to open_end" do
+      Fabricate.build(:project, end_type: 'open_end').should_not have(1).errors_on(:end_type)
+    end
+
+    it "should be invalid if not inclusion in list" do
+      Fabricate.build(:project, end_type: 'invalid_end_type').should_not be_valid
+    end
+
+    context "#fixed_amount" do
+      it "should be valid if end_type eql to fixed_amount" do
+        Fabricate.build(:project, end_type: 'fixed_amount', fixed_amount: 10).should be_valid
+      end
+
+      it "should be invalid if not numerical" do
+        Fabricate.build(:project, end_type: 'fixed_amount', fixed_amount: 'test').should have(1).errors_on(:fixed_amount)
+      end
+
+      it "should be nil if end_type eql to open_end" do
+        project = Fabricate.build(:project, end_type: 'open_end', fixed_amount: 10, open_end: DateTime.now + 1.years)
+        project.save
+        project.fixed_amount.should be_nil
+      end
+    end
+
+    context "#open_end" do
+      it "should be valid if end_type eql to open_end" do
+        Fabricate.build(:project, end_type: 'open_end', open_end: DateTime.now + 1.years).should be_valid
+      end
+
+      it "should be invalid if not date" do
+        expect { Fabricate.build(:project, end_type: 'open_end', open_end: 'test') }.should raise_error(Mongoid::Errors::InvalidTime)
+      end
+
+      it "should be invalid if earlier that now" do
+        Fabricate.build(:project, end_type: 'open_end', open_end: DateTime.now - 1.years).should have(1).errors_on(:open_end)
+      end
+
+      it "should be nil if end_type eql to fixed_amount" do
+        project = Fabricate.build(:project, end_type: 'fixed_amount', fixed_amount: 10, open_end: DateTime.now + 1.years)
+        project.save
+        project.open_end.should be_nil
+      end
+    end
+  end
 end
