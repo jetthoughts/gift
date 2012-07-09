22268992f3c8456cee6f8c8dc06b0f069a6e184f
Maxim Litvinov <mli@jetthoughts.com>
2012-07-09 13:30:34 +0300

    refactor projects with helpers

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 feature "Project create and show" do
+  include RequestHelper
   background do
-    @user = Fabricate(:user)
-    visit root_path
+    sign_in
   end
 
   scenario "Create project with valid fixed amount" do
-    sign_in
-
     click_link 'New Project'
 
     page.should have_content 'New project'
@@ -17,20 +15,16 @@ feature "Project create and show" do
     fill_in 'Description', with: 'Project Description'
     fill_in 'Article link', with: 'http://google.com'
     choose('Earn up to fixed amount')
+
     find('#project_fixed_amount').should be_visible
+
     fill_in 'Fixed amount', with: '10'
     choose('Pay pal')
-    click_button 'Create Project'
 
-    page.should have_content 'Test Project'
-    page.should have_content 'Project Description'
-    page.should have_link 'Profile'
-    page.should have_link 'Edit project'
+    should_create_valid_project
   end
 
   scenario "Create project with valid open end" do
-    sign_in
-
     click_link 'New Project'
 
     page.should have_content 'New project'
@@ -39,26 +33,11 @@ feature "Project create and show" do
     fill_in 'Description', with: 'Project Description'
     fill_in 'Article link', with: 'http://google.com'
     choose('Gather with OpenEnd')
+
     find('#project_open_end').should be_visible
     fill_in 'Open end', with: (Time.now + 1.months).strftime('%d/%m/%Y %H:%M')
     choose('Pay pal')
-    click_button 'Create Project'
 
-    page.should have_content 'Test Project'
-    page.should have_content 'Project Description'
-    page.should have_link 'Profile'
-    page.should have_link 'Edit project'
-  end
-
-
-  def sign_in
-    @user.update_attribute :confirmed_at, Time.now
-
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    click_button 'Sign in'
-
-    page.should have_content I18n.t('devise.sessions.signed_in')
-    page.should have_content 'Listing projects'
+    should_create_valid_project
   end
 end
---
+++ spec/support/request_helper.rb
@@ -0,0 +1,42 @@
+module RequestHelper
+  def sign_in
+    @user = Fabricate(:user)
+    @user.update_attribute :confirmed_at, Time.now
+    visit root_path
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Sign in'
+
+    page.should have_content I18n.t('devise.sessions.signed_in')
+    page.should have_content 'Listing projects'
+  end
+
+  def create_project
+    click_link 'New Project'
+
+    page.should have_content 'New project'
+
+    fill_in 'Name', with: 'Test Project'
+    fill_in 'Description', with: 'Project Description'
+    fill_in 'Article link', with: 'http://google.com'
+    choose('Earn up to fixed amount')
+
+    find('#project_fixed_amount').should be_visible
+
+    fill_in 'Fixed amount', with: '10'
+    choose('Pay pal')
+
+    click_button 'Create Project'
+    should_create_valid_project
+  end
+
+  def should_create_valid_project
+    click_button 'Create Project'
+
+    page.should have_content 'Test Project'
+    page.should have_content 'Project Description'
+    page.should have_link 'Profile'
+    page.should have_link 'Edit project'
+  end
+end
