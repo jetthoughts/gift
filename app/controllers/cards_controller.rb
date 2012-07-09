452b3beae5902860b0cf9050d03b4173b7faa911
Maxim Litvinov <mli@jetthoughts.com>
2012-07-09 15:04:46 +0300

    update cards controller and add new action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,43 @@
 class CardsController < ApplicationController
   def index
+    respond_with project, @cards = chain.all#, :layout => false
+  end
 
+  def new
+
+  end
+
+  def create
+    respond_with project, @card = chain.create(card_params.merge({user: current_user}))#, :layout => false
+  end
+
+  def update
+    respond_with project, @card = chain.update(params[:id], card_params)#, :layout => false
+  end
+
+  def destroy
+    respond_with project, @card = chain.destroy(params[:id])#, :layout => false
+  end
+
+  private
+
+  def card_params
+    params[:card].merge user: current_user
+  end
+
+  def chain
+    project.cards
+  end
+
+  def author
+    @author ||= params[:user_id] ? User.find(params[:user_id]) : nil
+  end
+
+  def projects_chain
+    @projects_chain ||= (author and author.projects) or Project
+  end
+
+  def project
+    projects_chain.find(params[:project_id])
   end
 end
