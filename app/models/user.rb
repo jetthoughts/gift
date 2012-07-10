39f5acfbb3bf2147f5acfef6a9d7bfaba5baaf64
Maxim Litvinov <mli@jetthoughts.com>
2012-07-10 13:26:35 +0300

    add votes controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Mongo::Voter
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
