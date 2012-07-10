7c414ae883eca99e26f71087f18580cdf2d4e82c
Maxim Litvinov <mli@jetthoughts.com>
2012-07-10 14:34:49 +0300

    like/dislike buttons

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,6 @@ class VotesController < ApplicationController
   before_filter :require_card
 
   def create
-    #params[:vote] -> #:votee_class => Post, :votee_id => post_id
     current_user.vote(@card, :up)
     redirect_to :back
   end
