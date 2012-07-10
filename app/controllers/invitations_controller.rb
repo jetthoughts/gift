9154543478da3326b655428bb1d3f26d7d5283df
Maxim Litvinov <mli@jetthoughts.com>
2012-07-10 18:32:24 +0300

    add invite email form

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,23 @@
 class InvitationsController < ApplicationController
-  def show
+  def index
+    @project = project
+  end
 
+  def create
+
+  end
+
+  private
+
+  def author
+    @author ||= params[:user_id] ? User.find(params[:user_id]) : nil
+  end
+
+  def projects_chain
+    @projects_chain ||= (author and author.projects) or Project
+  end
+
+  def project
+    projects_chain.find(params[:project_id])
   end
 end
