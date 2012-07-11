0a379f1bd83978afbe099c0405d3cdf09aea1d8d
Maxim Litvinov <mli@jetthoughts.com>
2012-07-11 16:44:24 +0300

    made invite sends

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,8 +6,25 @@ class InvitationsController < Devise::InvitationsController
   end
 
   def create
-    #User.invite! email: "new_user@example.com", invited_by: current_user
-    super
+    p '#' * 20
+    @project = project
+    @user = current_user
+    invite_params = params[:user][:name].zip params[:user][:email]
+
+    invite_params.each do |param|
+      name, email = param
+      if name.blank? || email.blank?
+        next
+      end
+      @invite = User.invite!({name: name, email: email}, @user)
+
+      if @invite.errors
+        render :new
+        return
+      end
+    end
+
+    redirect_to project_path @project
   end
 
   def edit
