0403e813f6e5a637010a1b08c4ed92f13f4cc9ca
Maxim Litvinov <mli@jetthoughts.com>
2012-07-13 18:48:28 +0300

    setup activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Gift::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :projects do
     resources :comments
     resources :cards do
