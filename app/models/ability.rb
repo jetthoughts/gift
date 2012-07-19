d6ebd4bda5ee84f862d329a93eb9949c71fb5931
Sergey Andreyev <san@jetthoughts.com>
2012-07-19 14:58:49 +0300

    Add ability

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.nil?
+      cannot :manage, :all
+      can :create, User
+    else
+      cannot :manage, :all
+
+      can [:create, :update, :destroy], Invite, user_id: user.id
+      can :manage, Project, admin_id: user.id
+      can :manage, Comment,  user_id: user.id
+      can :manage, User,          id: user.id
+
+      can [:create, :destroy], Card, user_id: user.id
+    end
+  end
+end
