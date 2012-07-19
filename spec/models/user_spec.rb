2a6e251f3ab5d2fef4feb07bd721fe21f6285caf
Sergey Andreyev <san@jetthoughts.com>
2012-07-19 20:23:08 +0300

    Start test for ability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,16 @@ describe User do
   it { should validate_uniqueness_of(:email) }
 
   it { should validate_presence_of(:password) }
+
+  describe 'abilities' do
+    subject { ability }
+    let(:ability) { Ability.new(user)}
+
+    context "when is an guest" do
+      let(:user) { nil }
+
+      it { should be_able_to(:create, User) }
+      it { should_not be_able_to(:show, [Project, Comment, Invite, Card]) }
+    end
+  end
 end
