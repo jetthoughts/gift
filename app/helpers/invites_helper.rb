b4a1825bee357fb3809de45402f871dd97b58028
Maxim Litvinov <mli@jetthoughts.com>
2012-07-23 15:48:58 +0300

    set already invited ids

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,2 +1,9 @@
 module InvitesHelper
+  def invite_ids project
+    invites = []
+    project.invites.each do |invite|
+      invites << invite.fb_id unless invite.fb_id.blank?
+    end
+    invites
+  end
 end
