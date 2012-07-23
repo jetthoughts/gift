c525064efa0617aa0159dc923ed3a8161d592473
Maxim Litvinov <mli@jetthoughts.com>
2012-07-23 12:25:19 +0300

    add permission to :create_facebook action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   load_and_authorize_resource unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
+    logger.debug 'CanCan AccessDenied: ' + exception.message
     redirect_to root_url, alert: exception.message
   end
 
