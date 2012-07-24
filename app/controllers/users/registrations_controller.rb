88b5e95b86cae9b3341d8a16d5b4bb672c559e8e
Alexey Kisel <aki@jetthoughts.com>
2012-07-24 16:07:01 +0300

    Add notification checkbox, changeing email without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,10 +14,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def profile
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
     new_attrs = params[resource_name]
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
     if resource.update_attributes(new_attrs)
-      set_flash_message :notice, :updated
+      flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+      set_flash_message :notice, flash_key
       redirect_to after_update_path_for(resource)
     else
       render :edit
@@ -25,6 +28,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   private
+  
+  def update_needs_confirmation?(resource, previous)
+    resource.respond_to?(:pending_reconfirmation?) &&
+      resource.pending_reconfirmation? &&
+      previous != resource.unconfirmed_email
+  end
 
   def build_resource(*args)
     super
