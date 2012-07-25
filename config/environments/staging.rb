9dc1b01278cf58c942a598933434cfa6f3eb75a1
Sergey Andreyev <san@jetthoughts.com>
2012-07-25 19:36:16 +0300

    Disable logging from ASIN on staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -84,4 +84,5 @@ ASIN::Configuration.configure do |config|
   config.secret        = ENV['AWS_SECRET']
   config.key           = ENV['AWS_KEY']
   config.associate_tag = ENV['AWS_ASSOCIATE_TAG']
+  config.logger        = nil
 end
