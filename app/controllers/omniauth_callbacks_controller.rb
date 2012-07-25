2306c5caf78ba5f1a4d95021d3acb60d1ab47413
Maxim Litvinov <mli@jetthoughts.com>
2012-07-25 18:22:50 +0300

    fix facebook invite auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
+
+      if session[:back].present?
+        sign_in @user
+        redirect_to session[:back]
+      else
+        sign_in_and_redirect @user, :event => :authentication
+      end
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
