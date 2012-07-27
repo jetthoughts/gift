0cc4b28a2c6f7cb49c71026ad652e158a7f10f3f
Maxim Litvinov <mli@jetthoughts.com>
2012-07-27 13:58:31 +0300

    #55: show default image if image not uploaded

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,25 +17,26 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
+    #return '' if file.nil?
     model.class.to_s.underscore
   end
 
-  process :resize_to_fit => [400, 400]
+  process :resize_to_fit => [300, 300]
 
   version :tiny_thumb do
     process :resize_to_fill => [50, 50]
   end
 
   version :thumb do
-    process :resize_to_fill => [200, 200]
+    process :resize_to_fill => [150, 150]
+  end
+
+  def default_url
+    "fallback/" +  [version_name, "default.jpg"].compact.join('_')
   end
 
   def extension_white_list
     %w(jpg jpeg gif png)
-  end
-
-  def default_url
-    "/assets/fallback/default.jpg"
   end
 
   def filename
@@ -44,5 +45,4 @@ class ImageUploader < CarrierWave::Uploader::Base
     ext = CarrierWave::SanitizedFile.new(file).extension || :jpeg
     "#{model.id}.#{ext}"
   end
-
 end
