a0fb7b3844325d53c24a1e162e289cd22ee2488d
Maxim Litvinov <mli@jetthoughts.com>
2012-07-30 17:28:26 +0300

    #50: add additional info to paid type

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,6 +13,7 @@ class Project
   field :fixed_amount, type: Float
   field :deadline, type: DateTime
   field :paid_type, type: String
+  field :paid_info, type: Hash, default: nil
   field :end_type, type: String
   field :currency, type: String, default: 'EUR'
   field :closed, type: Boolean, default: false
