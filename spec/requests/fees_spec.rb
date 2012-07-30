ef6d8670b0fc6e4bba00e4dcc6132b6fbe2f2f10
Maxim Litvinov <mli@jetthoughts.com>
2012-07-30 14:33:36 +0300

    fee some specs

---
+++ spec/requests/fees_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+feature "Fees page" do
+  include RequestHelper
+  background do
+    sign_in
+    create_project
+    visit project_path(@project)
+  end
+
+  scenario 'should show errors in incorrect' do
+    click_link 'Engage'
+    page.should have_content 'How do you want to contribute?'
+
+    click_button 'Next'
+    page.should have_content 'Some errors were found, please take a look:'
+    page.should have_content "Payment method can't be blank"
+    page.should have_content "Amount can't be blank"
+  end
+
+  scenario 'test credit card dialog', js: true do
+    execute_rake('authorizedotnet:load_record')
+
+    click_link 'Engage'
+    page.should have_content 'CreditCard'
+
+    fill_in 'fee_amount', with: 20
+    choose('CreditCard')
+
+    click_button 'Next'
+    page.should have_button 'Submit'
+
+
+    fill_in 'creditcard_name', with: 'Test Test'
+    fill_in 'creditcard_number', with: '4007000000027'
+    fill_in 'creditcard_cvv', with: '123'
+    select '2015', :from => 'creditcard_expires_on_1i'
+
+    click_button 'Submit'
+    current_path.should eql project_path(@project)
+    page.should have_content '20.00 donations'
+  end
+
+  def execute_rake(task)
+    require 'rake'
+    rake = Rake::Application.new
+    Rake.application = rake
+    Rake::Task.define_task(:environment)
+    load "gems/nimbleshop_authorizedotnet-0.0.5/lib/tasks/authorize.rake"
+    rake[task].invoke
+  end
+
+end
