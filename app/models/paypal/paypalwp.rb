12d2884393fe8d29c835d8a7c3281673560da351
Alexey Kisel <aki@jetthoughts.com>
2012-07-31 18:15:20 +0300

    Improve fee calculator

--- app/models/paypal/paypalwp.rb
+++ app/models/paypal/paypalwp.rb
@@ -1,7 +1,7 @@
 module Paypal
   class Paypalwp < PaymentMethod
 
-     field :paypal_percent, :type => Float, :default => 0.029
+     field :paypal_percent, :type => Float, :default => 0.019
      field :transaction_fee, :type => Float, :default => 0.35
      field :refund_fee, :type => Float, :default => 1.0
      
@@ -10,7 +10,7 @@ module Paypal
     end
 
     def fee(amount)
-      paypal_percent*amount + transaction_fee
+      ((amount * paypal_percent / (1 - paypal_percent))*100).ceil.to_f/100  + transaction_fee
     end
 
   end
