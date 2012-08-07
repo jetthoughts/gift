21d86710fce230d0b17eceae0baef9891377caa6
Maxim Litvinov <mli@jetthoughts.com>
2012-08-07 13:28:33 +0300

    #49: fix cancan permisions for closed projects

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,28 +9,34 @@ class Ability
     else
       cannot :manage, :all
       can :manage, Attachment
-      can :create, Invite
+      can :create, Invite do |invite|
+        !invite.project.closed
+      end
       can [:show, :update, :destroy, :create_facebook, :find_invite, :facebook_update, :facebook_destroy], Invite, user_id: user.id
       can :show, Project do |project|
         user.projects.for_ids(project.id).exists?
       end
-      can :close, Project,  admin_id: user.id
-      can :manage, Project, admin_id: user.id
-      can :manage, Comment,  user_id: user.id
-      can [:show, :create], Comment do |comment|
+      can :close, Project, admin_id: user.id
+      can [:edit, :update], Project, closed: false
+      can [:show, :create, :new, :index], Project, admin_id: user.id
+
+      can :manage, Comment do |comment|
+        !comment.project.closed
+      end
+      can [:show], Comment do |comment|
         comment.project && comment.project.participant?(user)
       end
-      can :manage,  Withdraw
+      can :manage, Withdraw
       can :manage, Fee
       can :manage, User, id: user.id
       can :create_facebook, User, id: user.id
       can [:new, :amazon_search, :fetch], Card
       can :create, Card do |card|
         card.project && card.project.participants_add_own_suggestions &&
-          card.project.participant?(user)
+            card.project.participant?(user) && !card.project.closed
       end
       can [:show, :update], Card do |card|
-        card.project.participant?(user)
+        card.project.participant?(user) && !card.project.closed
       end
       can :destroy, Card, user_id: user.id
     end
